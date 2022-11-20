9c6c3ed23314cb091966e408177e88893128154f
Samuel Hufen <samuel@hufen.xyz>
2022-11-20 15:47:25 +0100

    Adapted DMXCVersions

--- scripts/models/dmxcversion.py
+++ scripts/models/dmxcversion.py
@@ -2,10 +2,11 @@ from enum import Enum
 
 
 class DMXCVersion(Enum):
-    DMXC31 = "3.1"
-    DMXC311 = "3.1.1"
+
     DMXC32 = "3.2"
     DMXC321 = "3.2.1"
+    DMXC322 = "3.2.2"
+    DMXC323 = "3.2.3"
 
     def __lt__(self, other):
         if self.__class__ is other.__class__:
